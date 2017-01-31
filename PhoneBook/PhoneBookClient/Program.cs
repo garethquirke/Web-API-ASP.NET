2e1eaa5665c0d52f563ebbbee56958f15a63a29d
garethquirke <gareth.quirke@gmail.com>
2017-01-31 19:21:13 +0000

    Client added

---
+++ PhoneBook/PhoneBookClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PhoneBookClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
