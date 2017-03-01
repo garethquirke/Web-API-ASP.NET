ec5fe3e64810e752f3990a544fbbc5d27e24ffe4
garethquirke <gareth.quirke@gmail.com>
2017-03-01 01:31:41 +0000

    guitar api & client _ 20%

---
+++ GuitarAPI/GuitarAPI/Models/Guitar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuitarAPI.Models
+{
+    public class Guitar
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Make { get; set; }
+        public bool IsNew { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ GuitarClient/GuitarClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarClient
+{
+    class Program
+    {
+        static string address = "http://localhost:53905/";
+
+
+
+        static async Task GetAllGuitars()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(address);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    Console.WriteLine("Retrieve all Guitars in stock \n");
+
+                    HttpResponseMessage response = await client.GetAsync("/api/guitar/");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var guitars = await response.Content.ReadAsAsync<ICollection<Guitar>>();
+                        foreach (var g in guitars)
+                        {
+                            Console.WriteLine("ID: " + g.ID + "Name: " + g.Name + "Make: " + g.Make + "Is new?: " + g.IsNew + "Stock: " + g.Stock);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(response.StatusCode + " " + response.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            GetAllGuitars().Wait();
+            Console.ReadLine();
+        }
+    }
+}
